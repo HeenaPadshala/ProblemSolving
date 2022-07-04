339a7f2bd191c64f6efd8d46317a7c26b251b211
Heena Padshala <heenagajera96@gmail.com>
2022-07-04 18:45:22 +0530

    Equalitycheck

---
+++ ProblemSolving/Models/EqualityCheck.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProblemSolving.Models
+{
+    public class EqualityCheck
+    {
+        public dynamic input1 { get; set; }
+        public dynamic input2 { get; set; }
+        public string resultMessage { get; set; }
+        public bool result { get; set; }
+
+        public EqualityCheck CheckEqualityTest(EqualityCheck obj)
+        {
+            if ((input1.GetType() == input2.GetType()) && (input1.ToString() == input2.ToString()))
+            {
+                obj.resultMessage = "The type and value are equal.";
+                obj.result = true;
+                return obj;
+            }
+            else if ((input1.GetType() != input2.GetType()) && (input1.ToString() != input2.ToString()))
+            {
+                obj.resultMessage = "The type and value are different.";
+            }
+            else if (input1.GetType() != input2.GetType())
+            {
+                obj.resultMessage = "The type is different.";
+            }
+            else if (input1.ToString() != input2.ToString())
+            {
+                obj.resultMessage = "The value are different.";
+            }
+            obj.result = false;
+            return obj;
+        }
+    }
+}
